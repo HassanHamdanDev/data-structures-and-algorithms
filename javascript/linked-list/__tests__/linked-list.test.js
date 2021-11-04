20710edf0a5769a9f4d98e83e35057a179667c0c
HassanHamdanDev <hassanhamdandev@gmail.com>
2021-11-04 20:30:52 +0200

    add

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,27 @@
 'use strict';
 
-// Require our linked list implementation
-const LinkedLilst = require('../index');
+const LinkedList = require('../lib/linked-list');
+let list = new LinkedList();
+
 
 describe('Linked List', () => {
-  it('works', () => {
-    expect(true).toBeTruthy();
+  it('empty linked list', () => {
+    expect(list.head).toEqual(null);
+  });
+  it('add new node to empty ll', () => {
+    list.insert(1);
+    expect(list.head.value).toBe(1);
+    expect(list.head.next).toBeNull();
+  });
+  it('add new node to  ll', () => {
+    list.insert(2);
+    expect(list.head.value).toBe(2);
+    expect(list.head.next.value).toBe(1);
+    expect(list.head.next.next).toBeNull();
+  });
+  it('make the linked list  string', () => {
+    expect(list.toString()).toBe('{2} -> {1} -> NULL');
   });
 });
+
+
