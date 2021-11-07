e1877353f6e11f380ace5ff56ee2ae202a29bbac
HassanHamdanDev <hassanhamdandev@gmail.com>
2021-11-07 17:52:24 +0200

    add

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -2,18 +2,18 @@
 
 const LinkedList = require('./lib/linked-list');
 
-const linkedList = new LinkedList(); 
+const linkedList = new LinkedList();
 
 linkedList.insert(1);
 linkedList.insert(2);
-linkedList.insretBefore(2,6);
-linkedList.insretBefore(1,5);
-linkedList.insretBefore(2,4);
-linkedList.insretAfter(6,3);
-linkedList.insretAfter(1,8);
-linkedList.insretAfter(5,7);
+linkedList.insertBefore(2, 6);
+linkedList.insertBefore(1, 5);
+linkedList.insertBefore(2, 4);
+linkedList.insertAfter(6, 3);
+linkedList.insertAfter(1, 8);
+linkedList.insertAfter(5, 7);
 let check2 = linkedList.includes(1);
 let check3 = linkedList.includes(11);
 let llString = linkedList.toString();
 
-console.log(linkedList, check2, check3, llString,linkedList.length);
+console.log(linkedList, check2, check3, llString, linkedList.length);
