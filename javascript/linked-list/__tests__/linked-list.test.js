64d681383860489c7caef8d7a52a3d362325c541
HassanHamdanDev <hassanhamdandev@gmail.com>
2021-11-07 17:48:16 +0200

    add

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -2,6 +2,7 @@
 
 const LinkedList = require('../lib/linked-list');
 let list = new LinkedList();
+let newList = new LinkedList();
 
 
 describe('Linked List', () => {
@@ -20,8 +21,42 @@ describe('Linked List', () => {
     expect(list.head.next.next).toBeNull();
   });
   it('make the linked list  string', () => {
-    expect(list.toString()).toBe('{2} -> {1} -> NULL');
+    expect(list.toString()).toBe('{ 2 } -> { 1 } -> NULL');
   });
 });
+
+describe('linked-list-insertions', () => {
+  it(' append new node to empty ll', () => {
+    newList.append(1);
+    expect(newList.head.value).toBe(1);
+    expect(newList.head.next).toBeNull();
+  });
+  it(' append new node to the end of ll', () => {
+    newList.append(2);
+    expect(newList.head.value).toBe(1);
+    expect(newList.head.next.value).toBe(2);
+    expect(newList.head.next.next).toBeNull();
+  });
+  it(' add a value before the value in the ll', () => {
+    expect(newList.toString()).toBe('{ 1 } -> { 2 } -> NULL');
+    newList.insertBefore(1, 3);
+    expect(newList.toString()).toBe('{ 3 } -> { 1 } -> { 2 } -> NULL');
+    newList.insertBefore(2, 4);
+    expect(newList.toString()).toBe('{ 3 } -> { 1 } -> { 4 } -> { 2 } -> NULL');
+    newList.insertBefore(4, 5);
+    expect(newList.toString()).toBe('{ 3 } -> { 1 } -> { 5 } -> { 4 } -> { 2 } -> NULL');
+    expect(newList.insertBefore(9, 20)).toBe('value not exist');
+  });
+  it(' add a value after value in the ll', () => {
+    newList.insertAfter(3, 10);
+    expect(newList.toString()).toBe('{ 3 } -> { 10 } -> { 1 } -> { 5 } -> { 4 } -> { 2 } -> NULL');
+    newList.insertAfter(2, 11);
+    expect(newList.toString()).toBe('{ 3 } -> { 10 } -> { 1 } -> { 5 } -> { 4 } -> { 2 } -> { 11 } -> NULL');
+    newList.insertAfter(5, 12);
+    expect(newList.toString()).toBe('{ 3 } -> { 10 } -> { 1 } -> { 5 } -> { 12 } -> { 4 } -> { 2 } -> { 11 } -> NULL');
+    expect(newList.insertAfter(15, 15)).toBe('value not exist');
+  });
+});
+
 
 
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -6,8 +6,14 @@ const linkedList = new LinkedList();
 
 linkedList.insert(1);
 linkedList.insert(2);
+linkedList.insretBefore(2,6);
+linkedList.insretBefore(1,5);
+linkedList.insretBefore(2,4);
+linkedList.insretAfter(6,3);
+linkedList.insretAfter(1,8);
+linkedList.insretAfter(5,7);
 let check2 = linkedList.includes(1);
-let check3 = linkedList.includes(3);
+let check3 = linkedList.includes(11);
 let llString = linkedList.toString();
 
-console.log(linkedList, check2, check3, llString);
+console.log(linkedList, check2, check3, llString,linkedList.length);
